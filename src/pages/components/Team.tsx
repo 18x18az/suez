0ca809210f0c8b6a7dd9a430762b7cd8567cf07f
Joshua Tenorio <joshliam09@gmail.com>
2023-01-05 10:57:19 -0700

    sadge

--- src/pages/components/Team.tsx
+++ src/pages/components/Team.tsx
@@ -1,9 +1,22 @@
-import { ITeams, ITeam, IPath } from "@18x18az/rosetta";
+import { ITeams, ITeam, IPath, IMatchList, MatchId, IMatchInfo, ISimpleMatchResult } from "@18x18az/rosetta";
 import { Fragment, useEffect, useState } from "react";
 import Waiting from "./Waiting";
 import { useParams } from "react-router-dom";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { AppBar, Button, Toolbar } from "@mui/material";
+import { bifrost } from "../../ws";
+
+// TODO: move to rosetta
+export interface TeamSchedule {
+    [key: MatchId]: IMatchInfo
+}
+
+export interface TeamResults {
+    [key: string]: ISimpleMatchResult
+}
+
+export interface TeamEventData {
+    schedule: TeamSchedule,
+    results: TeamResults
+}
 
 interface TeamProps {
     teams: ITeams | null
@@ -15,6 +28,7 @@ export const Team = (props: TeamProps) => {
 
     const { number } = useParams();
     const [team, setTeam] = useState<ITeam | null>(null);
+    const [data, setData] = useState<TeamEventData | null>(null);
 
     if (!team) {
         for (const property in props.teams) {
@@ -25,22 +39,24 @@ export const Team = (props: TeamProps) => {
         }
     }
 
+    if (!data) {
+        console.log("brrr");
+        bifrost.get(['team', number as string]);
+    }
+
+    if (props && props.lastMessagePath && props.lastMessagePath[0] === "team" && props.lastMessagePath.length >= 2) {
+        if (props.lastMessagePath[1] === number) {
+            setData(props.lastMessageBody);
+        }
+    }
 
     useEffect(() => {
 
     }, [])
 
-    if (team) {
+    if (team && data) {
         return (
             <Fragment>
-                {/*<AppBar position="sticky">
-                    <Toolbar sx={{ minHeight: '5000px' }}>
-                        <Button>
-                        <ChevronLeftIcon/>
-                        </Button>
-                    </Toolbar>
-                    </AppBar>*/
-                }
                 <h1>{team.number}</h1>
                 <p>Team page WIP ;)</p>
             </Fragment>
