844100c5bf7130bc5b2492236de3f3b6a585461b
Joshua Tenorio <joshliam09@gmail.com>
2023-01-05 13:32:53 -0700

    feat: ranking, skills, schedule in team page

--- src/pages/components/Team.tsx
+++ src/pages/components/Team.tsx
@@ -1,5 +1,9 @@
-import { ITeams, ITeam, IPath, IMatchList, MatchId, IMatchInfo, ISimpleMatchResult } from "@18x18az/rosetta";
+import { ITeams, ITeam, IPath, IMatchList, MatchId, IMatchInfo, ISimpleMatchResult, ISkillsRankingData, IRankingData, ISkillsRankings, IRankings } from "@18x18az/rosetta";
 import { Fragment, useEffect, useState } from "react";
+import { Accordion, AccordionSummary, AccordionDetails, Table, TableBody, TableRow, TableCell, TableHead } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
+import { Match } from "./event/Match";
 import Waiting from "./Waiting";
 import { useParams } from "react-router-dom";
 import { bifrost } from "../../ws";
@@ -29,6 +33,8 @@ export const Team = (props: TeamProps) => {
     const { number } = useParams();
     const [team, setTeam] = useState<ITeam | null>(null);
     const [data, setData] = useState<TeamEventData | null>(null);
+    const [skills, setSkills] = useState<ISkillsRankings | null>(null);
+    const [rankings, setRankings] = useState<IRankings | null>(null);
 
     if (!team) {
         for (const property in props.teams) {
@@ -40,25 +46,176 @@ export const Team = (props: TeamProps) => {
     }
 
     if (!data) {
-        console.log("brrr");
         bifrost.get(['team', number as string]);
     }
 
-    if (props && props.lastMessagePath && props.lastMessagePath[0] === "team" && props.lastMessagePath.length >= 2) {
-        if (props.lastMessagePath[1] === number) {
-            setData(props.lastMessageBody);
-        }
+    if (!skills) {
+        bifrost.get(['skills']);
+    }
+
+    if (!rankings) {
+        bifrost.get(['rankings']);
     }
 
     useEffect(() => {
+        if (props && props.lastMessagePath && props.lastMessagePath[0] === "team" && props.lastMessagePath.length >= 2) {
+            if (props.lastMessagePath[1] === number) {
+                setData(props.lastMessageBody);
+            }
+        }
+        else if (props && props.lastMessagePath && props.lastMessagePath[0] === "rankings") {
+            setRankings(props.lastMessageBody);
+        }
+        else if (props && props.lastMessagePath && props.lastMessagePath[0] === "skills") {
+            setSkills(props.lastMessageBody)
+        }
+    }, [props])
 
-    }, [])
+    if (team && data && rankings && skills && props.teams) {
+        // rankings section
+        let rankingsOutput = <div></div>
 
-    if (team && data) {
+        rankings.forEach((ranking: IRankingData) => {
+            if (ranking.team === team.id) {
+                let pointsString = ranking.avgWP + " / ";
+                pointsString += ranking.avgAP + " / ";
+                pointsString += ranking.avgSP;
+                rankingsOutput = 
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">
+                                    Rank
+                                </TableCell>
+                                <TableCell align="center">
+                                    W-L-T
+                                </TableCell>
+                                <TableCell align="center">
+                                    Average WP / AP / SP
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell align="center">
+                                    {ranking.rank}
+                                </TableCell>
+                                <TableCell align="center">
+                                    {ranking.record}
+                                </TableCell>
+                                <TableCell align="center">
+                                    {pointsString}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+
+            }
+        });
+
+        // skills section
+        let skillsOutput = 
+                <p>No Robot Skills attempts found, 3 Driver and 3 Programming attempts remaining</p>;
+        skills.forEach((skillsData: ISkillsRankingData) => {
+            if (skillsData.team === team.id) {
+                skillsOutput = <div>
+                    <p>Driver Attempts left: {3-skillsData.numDriver}</p>
+                    <p>Programming Attempts left: {3-skillsData.numProgramming}</p>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">
+                                    Rank
+                                </TableCell>
+                                <TableCell align="center">
+                                    Total Score
+                                </TableCell>
+                                <TableCell align="center">
+                                    Highest Programming
+                                </TableCell>
+                                <TableCell align="center">
+                                    Highest Driver
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        <TableRow>
+                                <TableCell align="center">
+                                    {skillsData.rank}
+                                </TableCell>
+                                <TableCell align="center">
+                                    {skillsData.total}
+                                </TableCell>
+                                <TableCell align="center">
+                                    {skillsData.highProgramming}
+                                </TableCell>
+                                <TableCell align="center">
+                                    {skillsData.highDriver}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                    </div>
+            }
+        });
+
+        // results section
+        let resultsOutput = 
+                <p>No match results found!</p>;
+
+        let matches: IMatchInfo[] = [];
+        for (const property in data.schedule) {
+            matches.push(data.schedule[property])
+        }
+        let scheduleOutput = [];
+        if (matches) {
+            for (let i = 0; i < matches.length; i++) {
+                const match = matches[i];
+                const matchItem = <Match teams={props.teams} match={match} />
+                scheduleOutput.push(matchItem);
+            }
+        }
         return (
             <Fragment>
-                <h1>{team.number}</h1>
-                <p>Team page WIP ;)</p>
+                <h1 style={{ marginBottom: 0}}>{team.number}</h1>
+                <h2 style={{ marginTop: 0}}>{team.name}</h2>
+                <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    Ranking Data
+                </AccordionSummary>
+                <AccordionDetails>
+                {rankingsOutput}
+                </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    Robot Skills Data
+                </AccordionSummary>
+                <AccordionDetails>
+                {skillsOutput}
+                </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                        Match Results
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    {resultsOutput}
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion defaultExpanded={true}>
+                    <AccordionSummary  expandIcon={<ExpandMoreIcon/>}>
+                        Schedule
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Table>
+                            <TableBody>
+                            {scheduleOutput}
+                            </TableBody>
+                        </Table>
+                    </AccordionDetails>
+                </Accordion>
+                
             </Fragment>
 
         )
